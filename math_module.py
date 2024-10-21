4873544680da8d3e95de2030764c471b456d341b
Ashok-del341 <ashoknaidu.motupalli@gmail.com>
2024-10-21 14:21:38 +0530

    Add files via upload

---
+++ math_module.py
@@ -0,0 +1,16 @@
+
+
+name = "Ashok"
+age = 27
+# f-string
+formatted_string = f"{name} is {age} years old."
+# str.format
+formatted_string2 = "{} is {} years old.".format('Ashok',27)
+
+original_string = "Hello, ASHOK!"
+lowercase_string = original_string.lower()
+print(lowercase_string)
+
+original_string = "Hello, ASHOK!"
+uppercase_string = original_string.upper()
+print(uppercase_string)
